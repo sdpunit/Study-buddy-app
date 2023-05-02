ca1a8c562937006ee2cc909edd14402899ca2175
Steven <u7108792@anu.edu.au>
2023-05-02 13:20:40 +1000

    started search

--- src/studybuddy/app/src/main/java/com/studybuddy/SearchActivity.java
+++ src/studybuddy/app/src/main/java/com/studybuddy/SearchActivity.java
@@ -3,15 +3,39 @@ package com.studybuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
-// have an xml setup with a collections of names which updates whenever a new user is added
-// auto update to display names when input is typed in. once 
+import java.net.URL;
+import java.util.ArrayList;
+
 public class SearchActivity extends AppCompatActivity {
+
+    public static ArrayList<Person> personList = new ArrayList<Person>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        setupData();
+        setupList();
+        setupOnClickListener();
+    }
+
+    private void setupData(){
+        //get singleton data structure
+        //get data from database
+
+    }
+
+    private void setupList(){
+        PersonAdaptor adaptor = new PersonAdaptor(getApplicationContext(), R.layout.person_cell, personList);
+        ListView.setAdapter(adaptor);
+
+    }
+
+    private void setupOnClickListener(){
+
     }
 
 }
