5372d6adfbfad329873c2b42b34b1872c1c13bda
Yanghe Dong <u7533843@anu.edu.au>
2023-05-11 01:11:03 +1000

    Update RBTree test

--- app/src/test/java/com/studybuddy/RBTreeTest.java
+++ app/src/test/java/com/studybuddy/RBTreeTest.java
@@ -10,29 +10,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RBTreeTest {
-    private final User[] users = {new User(6750912, "Emily Thompson"),
-            new User(7340286, "Ava Walker"),
-            new User(6142853, "James Anderson"),
-            new User(6425273, "James Anderson"),
-            new User(7533843, "Yanghe Dong"),
-            new User(6239587, "Olivia Turner"),
-            new User(7431068, "Michael Taylor"),
-            new User(7442688, "Michael Tayer"),
-            new User(7452667, "Michael Taylor"),
-            new User(7648392, "Sophia Lewis"),
-            new User(6953424, "William White"),
-            new User(6750912, "Benjamin Harris"),
-            new User(7565334, "Amelia Martin"),
-            new User(7125906, "Samuel Jackson"),
-            new User(7486251, "Zihan Li"),
-            new User(7546283, "Zihan Li")};
+    private final Course[] courses = {new Course("COMP2300"), new Course("COMP3600"),
+            new Course("COMP2100"), new Course("COMP2100"), new Course("COMP4600"),
+            new Course("COMP3620"), new Course("COMP2420"), new Course("COMP3310"),
+            new Course("COMP4528"), new Course("COMP1110"), new Course("COMP2620"),
+            new Course("COMP4691"), new Course("COMP3630"), new Course("COMP3425")};
 
     private boolean compareLists(List<RBTree.Node> expected, List<RBTree.Node> actual) {
         if (expected.size() != actual.size()) {
             return false;
         }
         for (int i = 0; i < expected.size(); i++) {
-            if (!expected.get(i).getUser().equals(actual.get(i).getUser())) {
+            if (!expected.get(i).getCourse().equals(actual.get(i).getCourse())) {
                 return false;
             }
         }
@@ -53,7 +42,7 @@ public class RBTreeTest {
     public List<RBTree.Node> findLeaf(RBTree.Node node) {
         List<RBTree.Node> leaf = new ArrayList<>();
         if (node == null) return leaf;
-        if (node.left == null && node.right == null && node.getUser() == null) {
+        if (node.left == null && node.right == null && node.getCourse() == null) {
             leaf.add(node);
         }
         leaf.addAll(findLeaf(node.left));
@@ -115,16 +104,22 @@ public class RBTreeTest {
         this.tree = null;
     }
 
+
+    @Test(timeout = 1000)
+    public void testEmptyTree() {
+        assertNull(this.tree.root);
+    }
+
     @Test(timeout = 1000)
     public void testSimpleInsertWithoutReconstruction() {
         List<RBTree.Node> expected = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-            this.tree.insert(users[i]);
+            this.tree.insert(courses[i]);
         }
         List<RBTree.Node> actual = this.tree.inOrderTraverse();
-        expected.add(new RBTree.Node(users[1]));
-        expected.add(new RBTree.Node(users[0]));
-        expected.add(new RBTree.Node(users[2]));
+        expected.add(new RBTree.Node(courses[2]));
+        expected.add(new RBTree.Node(courses[0]));
+        expected.add(new RBTree.Node(courses[1]));
 
         assertTrue(testProp1(this.tree) && testProp2(this.tree) && testProp3(this.tree));
         assertTrue(compareLists(expected, actual));
@@ -134,13 +129,12 @@ public class RBTreeTest {
     public void testDuplicateInsert() {
         List<RBTree.Node> expected = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
-            this.tree.insert(users[i]);
+            this.tree.insert(courses[i]);
         }
         List<RBTree.Node> actual = this.tree.inOrderTraverse();
-        expected.add(new RBTree.Node(users[1]));
-        expected.add(new RBTree.Node(users[0]));
-        expected.add(new RBTree.Node(users[2]));
-        expected.add(new RBTree.Node(users[3]));
+        expected.add(new RBTree.Node(courses[2]));
+        expected.add(new RBTree.Node(courses[0]));
+        expected.add(new RBTree.Node(courses[1]));
 
         assertTrue(testProp1(this.tree) && testProp2(this.tree) && testProp3(this.tree));
         assertTrue(compareLists(expected, actual));
@@ -149,19 +143,17 @@ public class RBTreeTest {
     @Test(timeout = 1000)
     public void testMediumInsert() {
         List<RBTree.Node> expected = new ArrayList<>();
-        for (int i = 0; i < 9; i++) {
-            this.tree.insert(users[i]);
+        for (int i = 0; i < 8; i++) {
+            this.tree.insert(courses[i]);
         }
         List<RBTree.Node> actual = this.tree.inOrderTraverse();
-        expected.add(new RBTree.Node(users[1]));
-        expected.add(new RBTree.Node(users[0]));
-        expected.add(new RBTree.Node(users[2]));
-        expected.add(new RBTree.Node(users[3]));
-        expected.add(new RBTree.Node(users[7]));
-        expected.add(new RBTree.Node(users[6]));
-        expected.add(new RBTree.Node(users[8]));
-        expected.add(new RBTree.Node(users[5]));
-        expected.add(new RBTree.Node(users[4]));
+        expected.add(new RBTree.Node(courses[2]));
+        expected.add(new RBTree.Node(courses[0]));
+        expected.add(new RBTree.Node(courses[6]));
+        expected.add(new RBTree.Node(courses[7]));
+        expected.add(new RBTree.Node(courses[1]));
+        expected.add(new RBTree.Node(courses[5]));
+        expected.add(new RBTree.Node(courses[4]));
 
         assertTrue(testProp1(this.tree) && testProp2(this.tree) && testProp3(this.tree));
         assertTrue(compareLists(expected, actual));
@@ -170,45 +162,34 @@ public class RBTreeTest {
     @Test(timeout = 1000)
     public void testComplexInsert() {
         List<RBTree.Node> expected = new ArrayList<>();
-        for (int i = 0; i < 16; i++) {
-            this.tree.insert(users[i]);
+        for (int i = 0; i < 14; i++) {
+            this.tree.insert(courses[i]);
         }
         List<RBTree.Node> actual = this.tree.inOrderTraverse();
-        expected.add(new RBTree.Node(users[12]));
-        expected.add(new RBTree.Node(users[1]));
-        expected.add(new RBTree.Node(users[11]));
-        expected.add(new RBTree.Node(users[0]));
-        expected.add(new RBTree.Node(users[2]));
-        expected.add(new RBTree.Node(users[3]));
-        expected.add(new RBTree.Node(users[7]));
-        expected.add(new RBTree.Node(users[6]));
-        expected.add(new RBTree.Node(users[8]));
-        expected.add(new RBTree.Node(users[5]));
-        expected.add(new RBTree.Node(users[13]));
-        expected.add(new RBTree.Node(users[9]));
-        expected.add(new RBTree.Node(users[10]));
-        expected.add(new RBTree.Node(users[4]));
-        expected.add(new RBTree.Node(users[14]));
-        expected.add(new RBTree.Node(users[15]));
+        expected.add(new RBTree.Node(courses[9]));
+        expected.add(new RBTree.Node(courses[2]));
+        expected.add(new RBTree.Node(courses[0]));
+        expected.add(new RBTree.Node(courses[6]));
+        expected.add(new RBTree.Node(courses[10]));
+        expected.add(new RBTree.Node(courses[7]));
+        expected.add(new RBTree.Node(courses[13]));
+        expected.add(new RBTree.Node(courses[1]));
+        expected.add(new RBTree.Node(courses[5]));
+        expected.add(new RBTree.Node(courses[12]));
+        expected.add(new RBTree.Node(courses[8]));
+        expected.add(new RBTree.Node(courses[4]));
+        expected.add(new RBTree.Node(courses[11]));
 
         assertTrue(testProp1(this.tree) && testProp2(this.tree) && testProp3(this.tree));
         assertTrue(compareLists(expected, actual));
     }
 
     @Test(timeout = 1000)
-    public void testSearchByUniqueName() {
-        for (int i = 0; i < 16; i++) {
-            this.tree.insert(users[i]);
+    public void testSearchByCourseCode() {
+        for (int i = 0; i < 14; i++) {
+            this.tree.insert(courses[i]);
         }
-        assertEquals(this.tree.searchByName("Yanghe Dong").get(0).getUser(), users[4]);
+        assertEquals(this.tree.searchByCourseCode(this.tree.root,"COMP4600").getCourse(), courses[4]);
     }
 
-    @Test(timeout = 1000)
-    public void testSearchByDuplicateName() {
-        for (int i = 0; i < 16; i++) {
-            this.tree.insert(users[i]);
-        }
-        assertEquals(this.tree.searchByName("Michael Taylor").get(0).getUser(), users[6]);
-        assertEquals(this.tree.searchByName("Michael Taylor").get(1).getUser(), users[8]);
-    }
 }
