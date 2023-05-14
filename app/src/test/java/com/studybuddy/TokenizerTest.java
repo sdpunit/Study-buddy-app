6d3a90759bdfb658facb6512b7d849154b94ba27
Lana <u7103031@anu.edu.au>
2023-05-14 14:37:09 +1000

    started tokenizer tests

--- app/src/test/java/com/studybuddy/TokenizerTest.java
+++ app/src/test/java/com/studybuddy/TokenizerTest.java
@@ -14,19 +14,23 @@ public class TokenizerTest {
     private static Tokenizer tokenizer;
     private static final String A = "2100";
     private static final String B = "COMP";
-    private static final String C = "COMP 2100";
+    private static final String C = "COMP, 2100";
     private static final String D = "2100 Software Design Methodologies COMP";
     private static final String E = "COMP 2100 Software Design Methodologies, bernando";
+    private static final String F = "college= COMP";
     // add , as delimiter in search query or it might get too confusing.
 
 
     @Test(timeout=1000)
     public void testAddToken() {
-        tokenizer = new Tokenizer(A);
-        assertEquals("wrong token type", Token.Type.CODE, tokenizer.getCurrent().getType());
+//        tokenizer = new Tokenizer(A);
+//        assertEquals("wrong token type, current token is "+tokenizer.getCurrent().getType(), Token.Type.CODE, tokenizer.getCurrent().getType());
+//
+//        tokenizer = new Tokenizer(B);
+//        assertEquals("wrong token type, current token is "+tokenizer.getCurrent().getType(), COLLEGE, tokenizer.getCurrent().getType());
 
-        tokenizer = new Tokenizer(B);
-        assertEquals("wrong token type", COLLEGE, tokenizer.getCurrent().getType());
+//        tokenizer = new Tokenizer(F);
+//        assertEquals("wrong token type, current token is "+tokenizer.getCurrent().getType(), COLLEGE, tokenizer.getCurrent().getType());
 
         tokenizer = new Tokenizer(C);
         assertEquals("wrong token value", COLLEGE, tokenizer.getCurrent().getToken());
