b119861d8c2f1b54852f7421fbb2bd088db81b9e
Lana <u7103031@anu.edu.au>
2023-05-18 20:03:42 +1000

    created NotificationFactoryTest.java

---
+++ app/src/test/java/com/studybuddy/NotificationFactoryTest.java
@@ -0,0 +1,13 @@
+package com.studybuddy;
+
+import com.studybuddy.notification.NotificationFactory;
+
+import org.junit.Test;
+
+public class NotificationFactoryTest {
+
+    @Test(timeout=1000)
+    public void notificationTest() {
+
+    }
+}
